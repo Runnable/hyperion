34d17b13c9c02eb4a35c89bdf9b3bbf32a85911d
Casey Flynn <cflynn.us@gmail.com>
2015-10-18 15:19:51 -0700

    Add example initialization of Hyperion

---
+++ example/example.js
@@ -0,0 +1,40 @@
+/**
+ * Example initialization of Hyperion
+ *
+ * NODE_PATH=./server/lib node ./example.js
+ * @module example/example
+ */
+'use strict';
+
+var Hyperion = require('../index');
+
+var hyperion = new Hyperion({
+  port: 3000,
+  sequences: {
+    'build-system': {
+      checkpoints: [{
+        name: 'worker1-start',
+        intervalSeconds: 30
+      }, {
+        name: 'worker1-complete',
+        intervalSeconds: 5
+      }, {
+        name: 'microservice1-message-received',
+        intervalSeconds: 60
+      }, {
+        name: 'microservice1-completed'
+      }]
+    },
+    'deploy-system': {
+      checkpoints: [{
+        name: 'worker1a-start',
+        intervalSeconds: 30
+      }, {
+        name: 'worker1a-finish'
+      }]
+    }
+  }
+});
+
+hyperion.start();
+
