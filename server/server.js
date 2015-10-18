fb7fc2cdca3ea67e3048242bf485809803afdbbb
Casey Flynn <cflynn.us@gmail.com>
2015-10-18 13:58:56 -0700

    Add start prototype method to server module

--- server/server.js
+++ server/server.js
@@ -7,19 +7,30 @@
 var express = require('express');
 
 var log = require('logger')(__filename);
+var routes = require('routes/routes');
+var sequences = require('models/sequences');
 
 var app = express();
 var exports = module.exports = app;
 
-exports._expressListenCallback = (err) => {
+exports._expressListenCallback = (port, err) => {
   if (err) {
     return log.error({
       err: err
     }, '_expressListenCallback error');
   }
-  log.trace('_expressListenCallback success PORT: '+process.env.PORT);
+  log.trace('_expressListenCallback success PORT: '+
+            port);
 };
 
-
-
-exports.listen(process.env.PORT, exports._expressListenCallback);
+/**
+ * Initialize application.
+ *   - Initialize sequences
+ *   - Bind routes
+ *   - Start express server
+ */
+exports.start = (opts) => {
+  sequences.initialize(opts.sequences);
+  routes.initialize(app);
+  exports.listen(opts.port, exports._expressListenCallback.bind(this, opts.port));
+};
