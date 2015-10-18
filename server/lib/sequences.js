943314c3b47123ece4a3910761eba86691243c19
Casey Flynn <cflynn.us@gmail.com>
2015-10-18 10:13:10 -0700

    Add module and test module for sequences

---
+++ server/lib/sequences.js
@@ -0,0 +1,20 @@
+/**
+ * @module server/lib/sequences
+ */
+'use strict';
+
+var exports = module.exports;
+
+var sequences;
+
+exports.initialize = (sequences) => {
+};
+
+/**
+ * Return sequence from hash of registered sequences
+ * @param {sequence} string
+ * @return {Object}
+ */
+exports.getSequence = (sequence) => {
+
+};
