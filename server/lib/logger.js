595a6b10045bc3d0232979b018d457d21b3224d0
Casey Flynn <cflynn.us@gmail.com>
2015-10-17 17:19:23 -0700

    Modify logger module to export generator function

--- server/lib/logger.js
+++ server/lib/logger.js
@@ -18,8 +18,22 @@ var serializers = put(bunyan.stdSerializers, {
   /* custom serializers will go here */
 });
 
-module.exports = bunyan.createLogger({
+var logger = bunyan.createLogger({
   name: 'hyperion',
   streams: streams,
   serializers: serializers
 });
+
+/**
+ * https://github.com/trentm/node-bunyan#logchild
+ * Initialize and return instance of a bunyan child logger
+ * @param {String} module
+ * @return {Object} instance of bunyan child logger
+ */
+function createChildLogger (module) {
+  return logger.child({
+    module: module
+  });
+}
+
+module.exports = createChildLogger;
