71fa5048859fa9171dc436ac33e65d2eb1c0729c
Casey Flynn <cflynn.us@gmail.com>
2015-10-20 23:35:54 -0700

    Add initialization of socket.io to server

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@
  */
 'use strict';
 
+var socketIO = require('socket.io');
 var bodyParser = require('body-parser');
 var express = require('express');
+var http = require('http');
 var mongoose = require('mongoose');
 var path = require('path');
 
@@ -17,6 +19,8 @@ var sequences = require('models/sequences');
 
 var app = express();
 var exports = module.exports = app;
+var server = http.Server(app);
+var io = socketIO(server);
 
 exports._expressListenCallback = (port, err) => {
   if (err) {
@@ -64,5 +68,15 @@ exports.start = (opts) => {
   routesClient.initialize(app);
   routesSequences.initialize(app);
 
-  exports.listen(opts.port, exports._expressListenCallback.bind(this, opts.port));
+  io.on('connection', function (socket) {
+    console.log('connection!');
+    setInterval(function () {
+      socket.emit('data', Math.ceil(Math.random()*100));
+    }, 1000);
+    socket.on('data', function (data) {
+      console.log('socketData', data);
+    });
+  });
+
+  server.listen(opts.port, exports._expressListenCallback.bind(this, opts.port));
 };
