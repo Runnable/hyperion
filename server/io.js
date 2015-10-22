bc9ccc293d06398bb5263dc9db78c3d0365b9be0
Casey Flynn <cflynn.us@gmail.com>
2015-10-21 19:43:39 -0700

    Remove emit test logic from io module

--- server/io.js
+++ server/io.js
@@ -5,16 +5,11 @@
 
 var socketIO = require('socket.io');
 
+var log = require('lib/logger')(__filename);
 var server = require('server');
 
 var io = module.exports = socketIO(server);
 
 io.on('connection', function (socket) {
-  console.log('connection!');
-  setInterval(function () {
-    socket.emit('data', Math.ceil(Math.random()*100));
-  }, 1000);
-  socket.on('data', function (data) {
-    console.log('socketData', data);
-  });
+  log.trace(socket, 'connection');
 });
