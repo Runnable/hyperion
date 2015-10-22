8218871eed2583c1f166b27081c115932458185c
Casey Flynn <cflynn.us@gmail.com>
2015-10-21 20:59:50 -0700

    Register config file routes with angular router

---
+++ client/router.js
@@ -0,0 +1,22 @@
+/**
+ * @module client/router
+ */
+'use strict';
+
+var app = require('./app');
+var routes = require('./config/routes');
+
+app.config([
+  '$routeProvider',
+  //'$locationProvider'
+function (
+  $routeProvider
+  //$locationProvider
+) {
+  routes.forEach(function (route) {
+    $routeProvider.when(route.url, {
+      templateUrl: route.templateUrl,
+      controller: route.controller
+    });
+  });
+}]);
