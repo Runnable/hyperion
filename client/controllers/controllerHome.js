8d0ef9c1d4f1a92f5e709df5af3b6319d979ce8e
Casey Flynn <cflynn.us@gmail.com>
2015-10-24 15:32:20 -0700

    Add test http request to home controller

--- client/controllers/controllerHome.js
+++ client/controllers/controllerHome.js
@@ -7,7 +7,16 @@ require('../app')
   .controller('controllerHome', controllerHome);
 
 function controllerHome (
-  $scope
+  $scope,
+  $http
 ) {
   console.log('controllerHome');
+
+  $http.get('/api/sequences')
+  .then(function success (response) {
+    console.log('success', response);
+  }, function failure (response) {
+    console.log('failure', response);
+  });
+
 }
