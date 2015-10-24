c7a64319c23dc2c4f6809a8911b6e0cb3dd787d8
Casey Flynn <cflynn.us@gmail.com>
2015-10-24 15:41:23 -0700

    Add implementation of socket service

--- client/services/socket.js
+++ client/services/socket.js
@@ -2,3 +2,13 @@
  * @module client/services/socket
  */
 'use strict';
+
+var io = require('./lib/socket.io');
+var socket = io.connect('http://localhost:3000');
+
+require('app')
+  .factory('socket', socketFactory);
+
+function socketFactory () {
+  return socket;
+}
