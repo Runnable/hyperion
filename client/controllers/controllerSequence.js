502377dfadd7e958a9e701f96f6a807e4c955da8
Casey Flynn <cflynn.us@gmail.com>
2015-10-24 19:51:07 -0700

    Add implementation for controllerSequence

--- client/controllers/controllerSequence.js
+++ client/controllers/controllerSequence.js
@@ -3,11 +3,47 @@
  */
 'use strict';
 
+// TODO move to service
+var find = require('101/find');
+
 require('app')
   .controller('controllerSequence', controllerSequence);
 
 function controllerSequence (
+  $http,
+  $routeParams,
   $scope
 ) {
-  console.log('controllerSequence');
+  var data = $scope.sequenceData = {};
+
+  data.sequenceName = $routeParams.sequenceName;
+  data.sequenceUUid = $routeParams.sequenceUuid;
+
+  /**
+   * Test if spec checkpoint is in list of reached checkpoints
+   * @return {Boolean}
+   */
+  data.checkpointComplete = function (checkpointName) {
+    if (!data.sequence) { return false; }
+    return find(data.sequence.checkpoints, function (checkpoint) {
+      return checkpoint.name === checkpointName;
+    });
+  };
+
+  $http.get('/api/sequences')
+    .then(function success (res) {
+      data.sequencesSpecifications = res.data;
+      console.log(res.data, $routeParams.sequenceName);
+      data.sequenceSpec = res.data[$routeParams.sequenceName];
+      console.log(data.sequenceSpec);
+    },
+    function failure (res) {
+    });
+
+  $http.get('/api/sequences/'+$routeParams.sequenceName+'/'+$routeParams.sequenceUuid)
+    .then(function success (res) {
+      data.sequence = res.data;
+    },
+    function failure (res) {
+    });
 }
